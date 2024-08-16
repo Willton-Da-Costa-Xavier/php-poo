2f14e7eb479b0cdbaa5a1f1c7be94ef48abf6c93
Willton-Da-Costa-Xavier <willtondacosta@gmail.com>
2024-08-16 21:39:08 +0200

    criei as classes e interface

---
+++ aula05/ControleRemoto.php
@@ -0,0 +1,20 @@
+<?php 
+
+    class ControleRemoto{
+        private $volume;
+        private $ligado;
+        private $tocando;
+
+        public function __construct()
+        {
+            $this->volume = 50;
+            $this->ligado = false;
+            $this->tocando = false;
+        }
+
+        
+
+
+    }
+
+?>
